589fc24d2869d9fde9b4ef187cd604fb6ebce77d
Luis Larrateguy <luis.larratguy@danasistemas.com.ar>
2013-10-04 00:30:05 -0300

    noticket: internacionalizacion, ejemplos de configuracion, REST propuestos.

--- gams2web.py
+++ gams2web.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request, jsonify
 from flask.ext.assets import Environment, Bundle
+from flask.ext.babel import Babel
+import json
 
 app = Flask(__name__)
 app.config['MODELS_SETTINGS'] = 'static/models.cfg'
+babel = Babel(app)
 
 # Assets
 assets = Environment(app)
@@ -25,6 +28,42 @@ assets.register('body_scripts', js)
 def index():
     return render_template("index.html")
 
+@babel.localeselector
+def get_locale():
+    # if a user is logged in, use the locale from the user settings
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    # otherwise try to guess the language from the user accept
+    # header the browser transmits.  We support de/fr/en in this
+    # example.  The best match wins.
+    return request.accept_languages.best_match(['es', 'en'])
+
+# Futuro metodo rest para obtener la lista de modelos
+@app.route('/models', methods=["GET"])
+def get_models():
+	return jsonify(json.loads(open('examples/models.json').read()))
+
+# Futuro metodo rest para obtener la informacion de un modelo (ejemplo con Sudoku, seria parametro string)
+@app.route('/models/<model>', methods=["GET"])
+def get_model(model):
+	# TODO: String format o sanitizar esto...
+	cont = open('examples/'+ model+ '.json').read()
+	data = json.loads(cont)
+	return jsonify(data)
+	
+# Stub para guardar una instancia de ejecucion de un modelo (ejemplo con Sudoku)
+@app.route('/models/<model>/instance', methods=["POST"])
+def save_model_instance(model):
+	return jsonify(json.loads(request.data))
+	
+# Stub para guardar una instancia de ejecucion de un modelo (ejemplo con Sudoku)
+@app.route('/models/<model>/instance/<instanceId>', methods=["GET"])
+def get_model_instance(model, instanceId):
+	data = json.loads(request.data)
+	return jsonify(data)
+    
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
