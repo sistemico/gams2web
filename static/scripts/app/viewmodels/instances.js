9ac61c35c92937d2174d58970740823ffb1ce324
Sebastián Galiano <sgaliano@gmail.com>
2013-10-17 22:54:50 -0300

    First version of app front-end based on Durandal 2.0

---
+++ static/scripts/app/viewmodels/instances.js
@@ -0,0 +1,13 @@
+﻿define(['plugins/http', 'knockout'], function (http, ko) {
+    return {
+        instances: ko.observableArray([]),
+
+        activate: function () {
+            var self = this;
+
+            return http.get('/api/instances').then(function(data) {
+                self.instances(data.instances);
+            });
+        }
+    };
+});
---
+++ static/scripts/app/viewmodels/models.js
@@ -0,0 +1,27 @@
+﻿define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+    return {
+        models: ko.observableArray([]),
+
+        activate: function () {
+            var self = this;
+
+            if (this.models().length > 0) {
+                return;
+            }
+
+            return http.get('/api/models').then(function(data) {
+                self.models(data.models);
+            });
+        },
+
+        canDeactivate: function () {
+            //return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+            return true
+        },
+
+        select: function(item) {
+            //item.viewUrl = 'views/detail';
+            //app.showDialog(item);
+        }
+    };
+});
