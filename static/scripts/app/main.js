4895d2776e0559d410dc3bdf0e293c7911efe873
sistemica <sgaliano@gmail.com>
2013-10-17 22:54:50 -0300

    First version of app front-end based on Durandal 2.0

---
+++ static/scripts/app/main.js
@@ -0,0 +1,40 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../vendor/require/text',
+        'durandal':'../vendor/durandal',
+        'plugins' : '../vendor/durandal/plugins',
+        'transitions' : '../vendor/durandal/transitions',
+        'knockout': '../vendor/knockout/knockout-2.3.0',
+        'bootstrap': '../vendor/bootstrap/bootstrap',
+        'jquery': '../vendor/jquery/jquery-2.0.3'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+       }
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'GAMS Web Interface';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true,
+        widget: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application.
+        app.setRoot('viewmodels/shell');
+    });
+});
---
+++ static/scripts/app/viewmodels/shell.js
@@ -0,0 +1,20 @@
+﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+    return {
+        router: router,
+
+        activate: function () {
+            router.map([
+                { route: ['', 'models'], title:'Models', moduleId: 'viewmodels/models', nav: true },
+                { route: 'instances', title:'Instances', moduleId: 'viewmodels/instances', nav: true },
+                { route: 'about', title:'About', moduleId: 'viewmodels/about', nav: true },
+                { route: 'contact', title:'Contact', moduleId: 'viewmodels/contact', nav: true }
+            ]).buildNavigationModel();
+            
+            return router.activate();
+        },
+
+        message: function() {
+            app.showMessage('Not yet implemented...');
+        }
+    };
+});
