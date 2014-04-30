5278ce1067add1c93443323706617ae84f754736
sistemica <sgaliano@gmail.com>
2014-04-30 00:44:02 -0300

    General improvements and adjustments. Integrating AngularJS with RequireJS.

--- static/app/main.js
+++ static/app/main.js
@@ -1,162 +1,141 @@
-﻿//
-// GAMS Web Interface
-//
+﻿define(['angular', 'angular-ui-router', 'angular-bootstrap', 'ng-i18next', 'app/data', 'app/controllers'], function (angular, uiRouter, uiBootstrap, i18next, data, controllers) {
+    angular.module('gams2web', [ 'ui.router', 'ui.bootstrap', 'jm.i18next'])
 
-var gams2web = angular.module('gams2web', [ 'ui.router', 'ui.bootstrap', 'jm.i18next']);
+        .run(function ($rootScope) {
+            $rootScope.data = data;
+        })
 
-// Matrix field type
-gams2web.directive('matrix', function () {
-    return {
-        templateUrl: 'views/fields/matrix.html',
-        restrict: 'E',
-        replace: true,
-        transclude: true,
-        scope: { nrows: '@', ncols: '@', val: '=value' },
+        .config(function ($locationProvider, $stateProvider, $urlRouterProvider, $i18nextProvider) {
+            $locationProvider.html5Mode(true);
+            $urlRouterProvider.otherwise('/');
 
-        link: function (scope, element, attrs) {
-            var range = function (upper) {
-                return Array.apply(null, Array(parseInt(upper))).map(function (_, i) {
-                    return i + 1 + '';
-                });
-            };
-
-            scope.rows = range(attrs.nrows);
-            scope.columns = range(attrs.ncols);
-
-            scope.value = {};
-
-            angular.forEach(scope.rows, function (row) {
-                scope.value[row] = {};
-
-                angular.forEach(scope.columns, function (column) {
-                    scope.value[row][column] = null;
-                });
-            });
-        }
-    }
-});
-
-
-// Create a copy of the array and reverse the order of the items
-gams2web.filter('reverse', function () {
-    return function (items) {
-        return items && items.slice().reverse() || [];
-    };
-});
-
-
-// App
-gams2web.config(function ($stateProvider, $locationProvider, $urlRouterProvider, $i18nextProvider) {
-    $locationProvider.html5Mode(true);
-
-    $i18nextProvider.options = {
-        lng: 'en',
-        fallbackLng: 'es',
-        detectLngQS: 'lang',
-        resGetPath: '../locales/__lng__/__ns__.json',
-        useCookie: false,
-        useLocalStorage: false
-    };
-
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'views/home.html',
-            controller: function ($scope, $rootScope, $modal, $log, $i18next) {
-                var addTask = $scope.addTask = function (model) {
-                    $rootScope.data.tasks.push({
-                        model: model.title,
-                        status: 'Queued'
-                    });
-                };
-
-                $scope.cancelTask = function (index) {
-                    $rootScope.data.tasks.splice(index, 1);
-                };
-
-                $scope.openRunModelDialog = function () {
-                    var modelSelectionDialog = $modal.open({
-                        templateUrl: 'views/modelSelectionDialog.html',
-                        controller: function ($scope, $modalInstance, models) {
-                            $scope.models = models;
-
-                            $scope.select = function (selectedModel) {
-                                $modalInstance.close(selectedModel);
-
-                                var modelParameterDialog = $modal.open({
-                                    templateUrl: 'views/modelParameterDialog.html',
-                                    backdrop: 'static',
-                                    windowClass: 'model-parameter-dialog',
-                                    controller: function ($scope, $modalInstance, model) {
-                                        $scope.model = model;
-
-                                        $scope.cancel = function () {
-                                            $modalInstance.dismiss('cancel');
-                                        }
-
-                                        $scope.run = function () {
-                                            $modalInstance.close(model);
-                                        }
-                                    },
-                                    resolve: {
-                                        model: function () {
-                                            return selectedModel;
-                                        }
-                                    }
-                                });
-
-                                modelParameterDialog.result.then(function (result) {
-                                    addTask(result);
-                                }, function () {
-                                    $log.info('Modal dismissed at: ' + new Date());
-                                });
-                            }
-
-                            $scope.cancel = function () {
-                                $modalInstance.dismiss('cancel');
-                            }
-                        },
-                        resolve: {
-                            models: function () {
-                                return $rootScope.data.models;
+            $stateProvider
+                .state('root', {
+                    url: '',
+                    abstract: true,
+                    views: {
+                        header: {
+                            templateUrl: 'views/navigation.html'
+                        }
+                    }
+                })
+                .state('root.home', {
+                    url: '/',
+                    views: {
+                        'content@': {
+                            templateUrl: 'views/dashboard.html',
+                            controller: function ($scope, $rootScope, $modal) {
+                                $scope.showModelListDialog = function () {
+                                    $modal.open({
+                                        templateUrl: 'views/model-list-dialog.html'
+                                    });
+                                };
                             }
                         }
+                    },
+                })
+                .state('root.about', {
+                    url: '/about',
+                    views: {
+                        'content@': {
+                            templateUrl: 'views/about.html'
+                        }
+                    }
+                })
+                .state('root.run', {
+                    url: '/{model}',
+                    views: {
+                        'content@': {
+                            templateUrl: 'views/model-run.html',
+                            controller: function ($scope, $rootScope, $state, $stateParams) {
+                                $scope.model = $rootScope.data.getModelInfo($stateParams.model);
+
+                                $scope.run = function () {
+                                    data.addTask($scope.model);
+                                    $state.go('root.home');
+                                }
+                            }
+                        }
+                    }
+                });
+
+            $i18nextProvider.options = {
+                lng: 'en',
+                fallbackLng: 'es',
+                detectLngQS: 'lang',
+                resGetPath: '../locales/__lng__/__ns__.json',
+                useCookie: false,
+                useLocalStorage: false
+            };
+        })
+
+        // WebSocket API
+        .factory('socket', function ($q) {
+            var self = {
+                socket: null,
+
+                connect: function () {
+                    self.socket = io.connect('/api');
+
+                    return self.on('connect');
+                },
+
+                on: function (event) {
+                    var deferred = $q.defer();
+
+                    self.socket.on(event, function (data) {
+                        deferred.resolve(data);
                     });
-                };
+
+                    return deferred.promise;
+                }
+            };
+
+            return self;
+        })
+
+        // Filter: Create a copy of the array and reverse the order of the items
+        .filter('reverse', function () {
+            return function (items) {
+                return items && items.slice().reverse() || [];
+            };
+        })
+
+        // Custom field: matrix
+        .directive('matrix', function () {
+            return {
+                templateUrl: 'views/fields/matrix.html',
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                scope: { nrows: '@', ncols: '@', val: '=value' },
+
+                link: function (scope, element, attrs) {
+                    var range = function (upper) {
+                        return Array.apply(null, Array(parseInt(upper))).map(function (_, i) {
+                            return i + 1 + '';
+                        });
+                    };
+
+                    scope.rows = range(attrs.nrows);
+                    scope.columns = range(attrs.ncols);
+
+                    scope.value = {};
+
+                    angular.forEach(scope.rows, function (row) {
+                        scope.value[row] = {};
+
+                        angular.forEach(scope.columns, function (column) {
+                            scope.value[row][column] = null;
+                        });
+                    });
+                }
             }
         })
 
-        .state('about', {
-            url: '/about',
-            templateUrl: 'views/about.html'
-        });
 
-    $urlRouterProvider.otherwise('/');
-});
-
-gams2web.run(function ($rootScope) {
-    $rootScope.data = {
-        models: [],
-        tasks: []
-    };
-
-    var socket = io.connect('/api');
-
-    socket.on('connect', function () {
-        socket.on('models.all', function (data) {
-            $rootScope.$apply(function () {
-                angular.forEach(data['models'], function (model) {
-                    $rootScope.data.models.push(model);
-                });
-            });
-        });
-
-        socket.on('tasks.all', function (data) {
-            $rootScope.$apply(function () {
-                angular.forEach(data['tasks'], function (task) {
-                    $rootScope.data.tasks.push(task);
-                });
-            });
-        });
+    // Bootstrapping
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ['gams2web']);
     });
 });
