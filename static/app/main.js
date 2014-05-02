34ba584e189c22eaf5ca375ff2cda4d30ba29833
Sebastián Galiano <sgaliano@gmail.com>
2014-05-02 18:18:38 -0300

    Controllers and routing simplified. Added new about dialog.

--- static/app/main.js
+++ static/app/main.js
@@ -1,72 +1,69 @@
 ﻿define(['angular', 'angular-ui-router', 'angular-bootstrap', 'ng-i18next', 'app/data'], function (angular, uiRouter, uiBootstrap, i18next, data) {
-    angular.module('gams2web', [ 'ui.router', 'ui.bootstrap'/*, 'jm.i18next'*/])
+    angular
+        .module('gams2web', [ 'ui.router', 'ui.bootstrap'/*, 'jm.i18next'*/])
 
-        .run(function ($rootScope) {
-            $rootScope.data = data;
-        })
+        .controller('MainCtrl', ['$scope', '$state', '$modal', function ($scope, $state, $modal) {
+            $scope.showModelListDialog = function () {
+                $modal.open({ templateUrl: 'views/model-list-dialog.html' });
+            };
+
+            $scope.showAboutDialog = function () {
+                $modal.open({ templateUrl: 'views/about-dialog.html' });
+            };
+
+            $scope.run = function (model) {
+                data.addTask(model);
+                $state.go('home');
+            }
+        }])
 
         .config(function ($locationProvider, $stateProvider, $urlRouterProvider/*, $i18nextProvider*/) {
-            $locationProvider.html5Mode(true);
-            $urlRouterProvider.otherwise('/');
-
             $stateProvider
-                .state('root', {
-                    url: '',
-                    abstract: true,
-                    views: {
-                        header: {
-                            templateUrl: 'views/navigation.html'
-                        }
-                    }
-                })
-                .state('root.home', {
+                .state('home', {
                     url: '/',
                     views: {
+                        'header': {
+                            templateUrl: 'views/navigation.html'
+                        },
                         'content@': {
-                            templateUrl: 'views/dashboard.html',
-                            controller: function ($scope, $rootScope, $modal) {
-                                $scope.showModelListDialog = function () {
-                                    $modal.open({
-                                        templateUrl: 'views/model-list-dialog.html'
-                                    });
-                                };
-                            }
+                            templateUrl: 'views/dashboard.html'
                         }
                     },
                 })
-                .state('root.about', {
-                    url: '/about',
-                    views: {
-                        'content@': {
-                            templateUrl: 'views/about.html'
-                        }
-                    }
-                })
-                .state('root.run', {
-                    url: '/{model}',
+                .state('model', {
+                    url: '{modelName}',
+                    parent: 'home',
                     views: {
                         'content@': {
                             templateUrl: 'views/model-run.html',
-                            controller: function ($scope, $rootScope, $state, $stateParams) {
-                                $scope.model = $rootScope.data.getModelInfo($stateParams.model);
-
-                                $scope.run = function () {
-                                    data.addTask($scope.model);
-                                    $state.go('root.home');
-                                }
+                            controller: function ($scope, model) {
+                                $scope.model = model;
                             }
+                        }
+                    },
+                    resolve: {
+                        model: function ($stateParams) {
+                            return data.getModelInfo($stateParams.modelName);
                         }
                     }
                 });
 
+            $urlRouterProvider.otherwise('/');
+
+            $locationProvider.html5Mode(true);
+
             /*$i18nextProvider.options = {
-                lng: 'en',
-                fallbackLng: 'es',
-                detectLngQS: 'lang',
-                resGetPath: '../locales/__lng__/__ns__.json',
-                useCookie: false,
-                useLocalStorage: false
-            };*/
+             lng: 'en',
+             fallbackLng: 'es',
+             detectLngQS: 'lang',
+             resGetPath: '../locales/__lng__/__ns__.json',
+             useCookie: false,
+             useLocalStorage: false
+             };*/
+        })
+
+        .run(function ($rootScope) {
+            $rootScope.data = data;
         })
 
         // WebSocket API
@@ -131,8 +128,7 @@
                     });
                 }
             }
-        })
-
+        });
 
     // Bootstrapping
     angular.element(document).ready(function () {
