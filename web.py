121a57cb50372488a2a1d14ec82743268c3905ae
Sebastián Galiano <sgaliano@gmail.com>
2014-05-04 15:30:39 -0300

    App no longer depend on Flask-Assets

--- web.py
+++ web.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-from flask.ext.assets import Environment
 from flask.ext.socketio import SocketIO
 
 import api
@@ -10,20 +9,18 @@ import api
 app = Flask(__name__, static_url_path='')
 app.config.from_pyfile('settings.py')
 
-# Resources
-assets = Environment(app)
-
 # WebSocket
 socket = SocketIO(app)
-
-# Main page (with support to Angular HTML5 routes mode)
-app.add_url_rule('/', 'root', lambda: app.send_static_file('views/index.html'))
-app.add_url_rule('/<path>', 'index', lambda path: app.send_static_file('views/index.html'))
 
 # Endpoints
 socket.on_message('connect', api.connect, namespace='/api')
 
 
-# Start local web server
+@app.route('/')
+@app.route('/<path>')
+def index(path=None):
+    return app.send_static_file('views/index.html')
+
+
 if __name__ == '__main__':
     socket.run(app, host='0.0.0.0')
